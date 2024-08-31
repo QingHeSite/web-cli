beb1496f6d9c2d6f514452322b0b0b7993a5e717
wangtao <wangtao@variflight.com>
2024-08-31 15:08:29 +0800

    web cli init

---
+++ config/rollup.config.js
@@ -0,0 +1,40 @@
+import resolve from '@rollup/plugin-node-resolve'
+import path from 'node:path'
+import typescript from "rollup-plugin-typescript2";
+import babel from "rollup-plugin-babel";
+import commonjs from '@rollup/plugin-commonjs';
+import json from '@rollup/plugin-json';
+import  terser from '@rollup/plugin-terser';
+
+const env = process.env.NODE_ENV // umd 模式的编译结果文件输出的全局变量名称
+console.log('env', env)
+// console.log('路径',path.relative('/','/entry.ts' ))
+const config = {
+  // 入口
+  input: path.join('../src/index.ts'),
+  output:  [
+    {
+      file: 'dist/index.cjs.js',
+        format: 'cjs',
+    },
+    {
+      file: 'dist/index.esm.js',
+      format: 'es',
+    }
+  ],
+  plugins: [typescript(), babel({
+    // 过滤文件
+    exclude: "node_modules/**",
+  }), resolve(), commonjs(), json()],
+}
+if (env === 'production') {
+  config.plugins.push(terser({
+    compress: {
+      pure_getters: true,
+      unsafe: true,
+      unsafe_comps: true,
+      warnings: false
+    }
+  }))
+}
+export default config
---
+++ src/index.ts
@@ -0,0 +1,78 @@
+#!/usr/bin/env node
+import { program } from "commander";
+import inquirer from 'inquirer';
+
+interface MyAnswers {
+  name: string;
+  language: string;
+}
+
+program
+  .option('-d, --debug', 'output extra debugging')
+  .option('-s, --small', 'small pizza size')
+  .option('-p, --pizza-type <type>', 'flavour of pizza');
+
+// program.version('0.0.2');
+
+
+const questions: any = [
+  {
+    type: 'input',
+    name: 'name',
+    message: 'What is your name?',
+  },
+]
+inquirer
+  .prompt(questions)
+  .then((answers) => {
+    // Use user feedback for... whatever!!
+    console.log('answers', answers);
+
+  })
+  .catch((error) => {
+    console.log('error', error);
+
+    if (error.isTtyError) {
+      // Prompt couldn't be rendered in the current environment
+    } else {
+      // Something else went wrong
+    }
+  });
+// program.command('create <name>').action(name => {
+//   // 获取一些项目信息
+//   inquirer.prompt([
+//     {
+//       name: 'author',
+//       message: '你的名字是：'
+//     }
+//   ]).then(res => {
+//     // 拿到信息参数
+//     console.log('信息参数', res);
+
+//     const { author, version, description } = res
+//     const beginTime = new Date().getTime()
+//     // download(`LeoJ340/webpack-template`, `./${name}`, err => {
+//     //   const time = (new Date().getTime() - beginTime) / 1000
+//     //   console.log(err || `create project finish in ${time}s`)
+//     // })
+//   })
+// });
+
+
+
+
+
+
+program.parse();
+
+const options = program.opts();
+
+if (options.debug) console.log(options);
+if (options.small) console.log('- small pizza size');
+if (options.pizzaType) console.log(`- ${options.pizzaType}`);
+
+
+
+
+
+// console.log('脚手架运行!')
